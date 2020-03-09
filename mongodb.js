edd00bcb108a6d50d6117d05fa18df4b19a5fc31
SSS-Rating <gomez.nicholas96@gmail.com>
2020-03-09 14:23:47 -0700

    taking object id's a part to recognize what is happening on the mongodb level

--- mongodb.js
+++ mongodb.js
@@ -1,20 +1,67 @@
 // CRUD: create, read, update, & delete
 
-const mongodb = require('mongodb')
-const MongoClient =  mongodb.MongoClient
+const { MongoClient, ObjectID } =require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database!')
     }
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Nick',
-        age: 23
-    })
+    // db.collection('users').insertOne({
+    //     name: 'Mike',
+    //     age: 26
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'take out trash',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'meal-prep',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'shower',
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to add items')
+    //     }
+        
+    //     console.log(result.ops)
+    // })
 })
