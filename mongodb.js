3e80c9c0ceb8506ffd1e9f050d7d88d13ca228f6
nicholasgomez-dev <gomez.nicholas96@gmail.com>
2020-03-10 13:02:17 -0700

    messing with crud operations

--- mongodb.js
+++ mongodb.js
@@ -12,23 +12,19 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').findOne({ _id: new ObjectID("5e66b2095584aa2e28d092f5") }, (error, user) => {
-    //     if (error) {
-    //         return console.log('Unable to fetch')
-    //     }
-
-    //     console.log(user)
+    // db.collection('users').deleteMany({
+    //     age: 24
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
     // })
 
-    // db.collection('users').find({ age: 23 }).toArray((error, users) => {
-    //     console.log(users)
-    // })
-
-    db.collection('tasks').findOne({ _id: new ObjectID("5e66f61d79a54518281c64f0") }, (error, task) => {
-        console.log(task)
-    })
-
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        console.log(tasks)
+    db.collection('tasks').deleteOne({
+        description: "shower"
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
 })
