1e408c0f7cb9898540e90ac8644f60c2d547b209
nicholasgomez-dev <gomez.nicholas96@gmail.com>
2020-05-03 19:19:35 -0700

    finished routes for crud operations of resources

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 require('./db/mongoose') //this require will just ensure that the file runs
 const User = require('./models/user')
 const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 // Setting: express, port
 const app = express()
@@ -10,85 +12,10 @@ const port = process.env.PORT || 3000
 
 // Setting: all incoming json -> accessible object
 app.use(express.json())
-
-// User Endpoints
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-// Task Endpoints
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-
-    try {
-        await task.save()
-        res.status(201).send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const task = await Task.findById(_id)
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+app.use(userRouter)
+app.use(taskRouter)
 
 // Setting: server
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-//download postman, run mongodb, udemy lesson 89 17mins
