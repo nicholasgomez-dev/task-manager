8bcedf465eed82d8d1547b9dac59634c19cbe34e
nicholasgomez-dev <gomez.nicholas96@gmail.com>
2020-05-06 19:00:21 -0700

    adding authentication

--- src/index.js
+++ src/index.js
@@ -17,3 +17,19 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+
+const bcrypt = require('bcryptjs')
+//8 Rounds of hashing is best for secuirty and speed
+const myFunction = async () => {
+    const password = 'Red12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,10 @@
 // Importing: Mongoose, Validator
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-
-// Creating: User model
-const User = mongoose.model('User', {
+//Users property schema for db: just declaration, nothing happening
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -42,6 +42,20 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+//standard function needed bc 'this' keyword doesnt get bind with arrow function: runs before saving user to db
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+// Creates User in DB
+const User = mongoose.model('User', userSchema)
 
 
 // Exporting: User
