e1a3cbbdb90b1825cbf25331e2924ba7bb3cee95
nicholasgomez-dev <gomez.nicholas96@gmail.com>
2020-05-08 17:35:34 -0700

    installing jsonwwebtoken

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
-// Improting: Express, mongoose.js, User model
 const express = require('express')
-require('./db/mongoose') //this require will just ensure that the file runs
+require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
-// Setting: express, port
 const app = express()
 const port = process.env.PORT || 3000
 
-// Setting: all incoming json -> accessible object
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-// Setting: server
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
+const jwt = require('jsonwebtoken')
 
-const bcrypt = require('bcryptjs')
-//8 Rounds of hashing is best for secuirty and speed
 const myFunction = async () => {
-    const password = 'Red12345!'
-    const hashedPassword = await bcrypt.hash(password, 8)
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
+    console.log(token)
 
-    console.log(password)
-    console.log(hashedPassword)
-
-    const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
-    console.log(isMatch)
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
 }
 
 myFunction()
