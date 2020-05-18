8228a091e2a744cf09a8f1c1bc930ff280113387
nicholasgomez-dev <gomez.nicholas96@gmail.com>
2020-05-17 21:16:52 -0700

    adding uploading file feature

--- src/index.js
+++ src/index.js
@@ -6,17 +6,14 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is under maintenance check back soon.')
-// })
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
 
 app.use(express.json())
 app.use(userRouter)
@@ -25,19 +22,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-
-// const Task = require('./models/task')
-// const User = require('./models/user')
-
-// const main = async () => {
-//     // const task = await Task.findById('5ebe256f0b8c9e4ca8b1b335')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     // const user = await User.findById('5ebe240e851aaf0e44ab9234')
-//     // await user.populate('tasks').execPopulate()
-//     // console.log(user.tasks)
-// }
-
-// main()
