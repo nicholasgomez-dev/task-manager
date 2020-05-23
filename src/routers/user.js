1be6418bcd9a6bd41eeae277b3595f0879bcccbd
nicholasgomez-dev <gomez.nicholas96@gmail.com>
2020-05-23 12:22:42 -0700

    delete avatar route

--- src/routers/user.js
+++ src/routers/user.js
@@ -6,7 +6,6 @@ const router = new express.Router()
 
 //config upload
 const upload = multer({
-    dest: 'avatars',
     limits: {
         fileSize: 1000000
     },
@@ -103,10 +102,20 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 //Upload avatar
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.send()
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
 })
 
+//Delete avatar
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
 module.exports = router
