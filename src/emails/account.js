409c373577f5dde9b46f4b5772de07c91446eb2d
nicholasgomez-dev <gomez.nicholas96@gmail.com>
2020-05-29 17:00:44 -0700

    adding email notifications

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,1 +1,12 @@
-const sendgridAPIKey = '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'
+const sgMail = require('@sendgrid/mail')
+
+const sendgridAPIKey = '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'
+
+sgMail.setApiKey(sendgridAPIKey)
+
+sgMail.send({
+    to: 'gomez.nicholas96@gmail.com',
+    from: 'gomez.nicholas96@gmail.com',
+    subject: 'This is my first creation',
+    text: 'I hope this one actually gets to you.'
+})
